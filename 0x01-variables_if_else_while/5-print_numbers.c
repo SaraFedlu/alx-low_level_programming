3e91235432594f598c83e6eef165bd85fbb80cba
Sara <sfedlu28@gmail.com>
2022-06-05 14:57:19 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print lowercase alphabets
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+printf("%d", i);
+i++;
+}
+putchar('\n');
+return (0);
+}
