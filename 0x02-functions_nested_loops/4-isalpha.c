8e0abbbcbbbd6a268c55b0bc12e496c6586a2f3c
Sara <sfedlu28@gmail.com>
2022-06-08 07:13:34 -0700

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - check alpha
+ * @c: par to be checked
+ * Return: (0)
+ */
+int _isalpha(int c)
+{
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	return (1);
+else
+	return (0);
+}
