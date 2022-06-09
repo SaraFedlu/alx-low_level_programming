a4bda62fd19c717fcced1abee1a6b1fded5a7106
Sara <sfedlu28@gmail.com>
2022-06-09 05:17:16 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: (0)
+ */
+int main(void)
+{
+int k;
+unsigned long i, j, s;
+i = 0;
+j = 1;
+k = 0;
+while (k < 50)
+{
+s = i + j;
+printf("%lu", s);
+i = j;
+j = s;
+if (k == 49)
+	printf("\n");
+else
+	printf(",");
+k++;
+}
+return (0);
+}
