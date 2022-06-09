b1c3ae49434f6df5daa5c13b615303fa22f952c4
Sara <sfedlu28@gmail.com>
2022-06-09 05:29:34 -0700

    fibonacci2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return (0)
+ */
+int main(void)
+{
+unsigned long i, j, s;
+float sum;
+while (1)
+{
+s = i + j;
+if (s > 4000000)
+	break;
+if ((s % 2) == 0)
+	sum += s;
+i = j;
+j = s;
+}
+printf("%.0f\n", sum);
+return (0);
+}
