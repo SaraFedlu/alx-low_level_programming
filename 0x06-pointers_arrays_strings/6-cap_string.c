45a6628bfd0cc0b43ed20b56db45087fd424ed1a
Sara <sfedlu28@gmail.com>
2022-06-16 08:43:09 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,18 +13,18 @@ int i = 0;
 while (str[i])
 {
 if (str[i] == ' ' ||
- str[i] == '\t' ||
- str[i] == '\n' ||
- str[i] == ',' ||
- str[i] == ';' ||
- str[i] == '.' ||
- str[i] == '?' ||
- str[i] == '!' ||
- str[i] == '(' ||
- str[i] == '"' ||
- str[i] == ')' ||
- str[i] == '{' ||
- str[i] == '}')
+str[i] == '\t' ||
+str[i] == '\n' ||
+str[i] == ',' ||
+str[i] == ';' ||
+str[i] == '.' ||
+str[i] == '?' ||
+str[i] == '!' ||
+str[i] == '(' ||
+str[i] == '"' ||
+str[i] == ')' ||
+str[i] == '{' ||
+str[i] == '}')
 {
 if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 	str[i + 1] -= 32;
